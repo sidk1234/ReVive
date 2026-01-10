32be0f9382d74e14a0e2ee92656d683045265f3a
sid <kumar.sidharthram@gmail.com>
2026-01-09 20:00:30 -0600

    favicon

--- Layout.js
+++ Layout.js
@@ -7,6 +7,11 @@ export default function Layout({ children }) {
       <Head>
         <link rel="icon" href="/favicon.png" />
         <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicon-180.png" />
+        <link rel="icon" type="image/png" sizes="192x192" href="/favicon-192.png" />
+        <link rel="icon" type="image/png" sizes="512x512" href="/favicon-512.png" />
       </Head>
       <style>{`
         @import url('https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap');
